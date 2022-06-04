cad5f6a2a606c38dd05d9e881235e78d3ab87e1d
MayconDouglasGois <99056981+MayconDouglasGois@users.noreply.github.com>
2022-06-04 20:32:09 -0300

    adicionado o scrol no menu de description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,18 @@
-import {GetServerSideProps} from "next"
 import Head from 'next/head'
-import { stripe } from "../services/stripe"
+import React from "react"
 
+import {GetServerSideProps} from "next"
+import { stripe } from "../services/stripe"
 import Stripe from "stripe"
+
 import ButtonCart from "../components/ButtonCart"
 import ButtonCurchase from "../components/ButtonCurchase"
+import {AiFillCaretDown, AiFillCaretUp} from 'react-icons/ai'
 
 import style from "../styles/Home.module.scss"
-import React from "react"
-import DescriptinComponent from "../components/DescriptinComponent"
+
+
+
 
 interface Iproduct {
   product: {
@@ -21,70 +25,81 @@ interface Iproduct {
   }
 
 
-const Home = ({product}:Iproduct)=> {
-  return (
-   <>
+
+
+const Home = ({product}:Iproduct)=> (
+  <>
     <Head>
       <title>md-commerce | Home</title>
     </Head>
     <main>
-    
-    <section className={style.product}>
-      <h1>PRODUTOS</h1>
-      <div className={style.container}>
- 
 
-      {product.map((item)=>{
- 
-   return(
-        <div className={style.contant} key={item.id}>
+      <section className={style.product}>
+        <h1>PRODUTOS</h1>
+        <div className={style.container}>
 
-          <img src={item.image} alt="digitando" className={style.img} />
 
-          
+          {product.map((item) => {
+            // const [visible, setVisible] = React.useState(true)
 
-            <div className={style.boxtext}>
 
-            <h2>{item.price.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</h2>
-              <h2>{item.name}</h2>
-              <div className={style.Curchase}>
-                <ButtonCurchase/>
-                <ButtonCart checkCurchase={false}/>
-                {/* <DescriptinComponent descriptins={item.descriptin}/> */}
+            return (
+              <div className={style.contant} key={item.id}>
+
+                <img src={item.image} alt="digitando" className={style.img} />
+
+                <div className={style.boxtext}>
+
+                  <h2>{item.price.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</h2>
+                  <h2>{item.name}</h2>
+
+                  <div className={style.Curchase}>
+                    <ButtonCurchase />
+                    <ButtonCart checkCurchase={false} />
+                  </div>
+                  
+                  <span className={style.description}>{item.descriptin}</span>
+                  {/* <button onClick={() => { setVisible(!visible) } }>{visible ? <AiFillCaretUp /> : <AiFillCaretDown />}</button> */}
+                
+                </div>
               </div>
-              
-            </div>
-        </div>
-   )
- })}
+            )
+          })}
 
-      </div> 
-    </section>
+        </div>
+      </section>
     </main>
-   </>)
-}
+  </>)
+
+
+
+
+
+
 export const getServerSideProps:GetServerSideProps =async ()=> {
 
-const ObjectProduct = await stripe.prices.list({
-  limit: 6,
-  expand:["data.product"]
-});
 
-  const product= ObjectProduct.data.map((item: Stripe.Price)=>{
-    return (
-      {
-    id: item.id,
-    image:(item.product as Stripe.Product).images,
-    name:(item.product as Stripe.Product).name,
-    descriptin:(item.product as Stripe.Product).description,
-    price:(item.unit_amount as number) / 100,
+  const ObjectProduct = await stripe.prices.list({
+    limit: 6,
+    expand:["data.product"]
+  });
+
+
+    const product= ObjectProduct.data.map((item: Stripe.Price)=>{
+      return (
+        {
+          id: item.id,
+          image:(item.product as Stripe.Product).images,
+          name:(item.product as Stripe.Product).name,
+          descriptin:(item.product as Stripe.Product).description,
+          price:(item.unit_amount as number) / 100,
+    }
+    )})
+    return {
+      props:{
+        product,
+      }
   }
-  )})
-  return {
-  props:{
-    product,
-  }
-}
 }
 
 
